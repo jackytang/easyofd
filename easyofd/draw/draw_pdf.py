f3c43f1dd35952e5121a66b60449939d80fefe8c
JackyTang <jackytang01@gmail.com>
2025-12-18 10:26:32 +0800

    Fix the inconsistent resizeX/Y in the calculation of offset and font size.

--- easyofd/draw/draw_pdf.py
+++ easyofd/draw/draw_pdf.py
@@ -177,6 +177,8 @@ class DrawPDF():
 
                 }
 
+                resizeX = CTM_info["resizeX"]
+                resizeY = CTM_info["resizeY"]
             else:
                 CTM_info ={}
             x_list = self.cmp_offset(line_dict.get("pos")[0], X, DeltaX, text, CTM_info, dire="X")
